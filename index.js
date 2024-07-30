b42aedc9835351626f5930f78b025592bf57e182
Ziji <Zijipia@gmail.com>
2024-07-31 00:49:45 +0700

    undo temp fix

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ class ZiExtractor extends BaseExtractor {
 
     async deactivate() {
         this.protocols = [];
+        ZiExtractor.instance = null;
     }
 
     async validate(query, type) {
