fc732d56d099eeda5c3cb2a77f4a80adaf2fe70d
Ziji <Zijipia@gmail.com>
2024-08-01 16:06:02 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,11 @@
-import { BaseExtractor, SearchQueryType, Track, GuildQueueHistory, ExtractorInfo, ExtractorSearchContext, ExtractorStreamable, ExtractorExecutionContext } from 'discord-player';
+import { BaseExtractor, SearchQueryType, Track, GuildQueueHistory, ExtractorInfo, ExtractorSearchContext, ExtractorStreamable, ExtractorExecutionContext, YoutubeExtractor } from 'discord-player';
 import Innertube from 'youtubei.js';
+interface YoutubeExtractorInit {
+    createStream?: (ext: BaseExtractor<object>, url: string) => Promise<Readable | string>;
+}
 
 declare class ZiExtractor extends BaseExtractor<YoutubeExtractorInit> {
-    static identifier = "com.Ziji.discord-player.youtube-Zijiext";
+    static identifier: "com.Ziji.discord-player.youtube-Zijiext";
     static instance: ZiExtractor | null;
     innerTube: Innertube;
     activate(): Promise<void>;
@@ -10,9 +13,9 @@ declare class ZiExtractor extends BaseExtractor<YoutubeExtractorInit> {
     validate(query: string, type?: SearchQueryType | null | undefined): Promise<boolean>;
     bridge(track: Track, sourceExtractor: BaseExtractor | null): Promise<ExtractorStreamable | null>;
     handle(query: string, context: ExtractorSearchContext): Promise<ExtractorInfo>;
-    private _searchYouTube;
-    private handlePlaylist;
-    private handleVideo;
+    _searchYouTube(query: string, context: ExtractorSearchContext): Track<any>;
+    handlePlaylist(vid: Video, context: ExtractorSearchContext, pl?: Playlist): Track<any>;
+    handleVideo(vid: Video, context: ExtractorSearchContext, pl?: Playlist): Track<any>;
     getRelatedTracks(track: Track, history: GuildQueueHistory): Promise<ExtractorInfo>;
     stream(info: Track): Promise<ExtractorStreamable>;
     emptyResponse(): ExtractorInfo;
